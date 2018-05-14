eac1caa1f832bc3a8bedae723ce5a44f93627ffd
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-05-14 12:16:47 -0400

    finished key for min lab

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  i = nil
+  if name_hash == {}
+    return nil
+  else
+  name_hash.each do |key,value|
+    if i == nil || i >= value
+      i = value
+    end
+  end
+  end
+  name_hash.each do |key,value|
+    if i == value
+      return key
+    end
+  end
 end
